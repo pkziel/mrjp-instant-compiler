a3645db0a1c341b3d12ff5e0b562303f34308bee
Piotr Zieliński <pkziel@gmail.com>
2017-10-31 23:34:47 +0100

    minor changes, increased efficiency

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ all:
 	chmod +x insc_jvm
 	chmod +x insc_llvm
 	make -C src
+clean:
+	make clean -C src
+distclean:
+	make distclean -C src		
--- src/JVM.hs
+++ src/JVM.hs
@@ -1,4 +1,4 @@
-module Main where 
+module Main where
 
 import AbsInstant
 import LexInstant
@@ -10,10 +10,13 @@ type Store = [(Ident, Integer)]
 
 generate :: [Stmt] -> Store -> [String] -> [String]
 generate (x:s) store jvm = jvm ++ generate s store (exec x store)
-generate _ store jvm = jvm    
+generate _ store jvm = jvm
 
 exec :: Stmt -> Store -> [String]
-exec (SAss x exp) s = eval exp s ++ ["    istore " ++ show number]
+exec (SAss x exp) s = eval exp s ++
+    if number >= 0 && number <= 3
+        then ["    istore_" ++ show number]
+        else ["    istore " ++ show number]
     where Just number = lookup x s
 exec (SExp exp) s = eval exp s ++ ["    getstatic java/lang/System/out Ljava/io/PrintStream;", "    swap", "    invokevirtual java/io/PrintStream/println(I)V"]
 
@@ -23,15 +26,25 @@ eval exp s = case exp of
     ExpMul exp0 exp -> (swapBranchesIfNeeded exp0 exp s) ++ ["    imul"]
     ExpDiv exp0 exp -> eval exp0 s ++ eval exp s ++ ["    idiv"]
     ExpSub exp0 exp -> eval exp0 s ++ eval exp s ++ ["    isub"]
-    ExpLit n -> if n < 5 
-        then ["    iconst_" ++ show n]
-        else ["    bipush " ++ show n]
-    ExpVar x -> ["    iload " ++ show number]
+    ExpLit n ->
+        if n >= 0 && n <= 5
+            then ["    iconst_" ++ show n]
+            else if n == -1
+                then ["    iconst_m" ++ show n]
+                else if n >= -128 && n <= 127
+                    then ["    bipush " ++ show n]
+                    else if n >= -32768 && n <= 32767
+                        then ["    sipush " ++ show n]
+                        else ["    ldc " ++ show n]
+    ExpVar x ->
+        if number >= 0 && number <= 3
+            then ["    iload_" ++ show number]
+            else ["    iload " ++ show number]
         where Just number = lookup x s
 
 swapBranchesIfNeeded exp0 exp s = if (depth exp0) > (depth exp)
     then eval exp0 s ++ eval exp s
-    else eval exp s ++ eval exp0 s 
+    else eval exp s ++ eval exp0 s
 
 depth :: Exp -> Int
 depth (ExpLit _) = 1
@@ -39,15 +52,15 @@ depth (ExpVar _) = 1
 depth (ExpAdd x1 x2) = if a > b
     then max a (b + 1)
     else max b (a + 1)
-    where 
+    where
         a = depth x1
         b = depth x2
 depth (ExpMul x1 x2) = if a > b
     then max a (b + 1)
     else max b (a + 1)
-    where 
+    where
         a = depth x1
-        b = depth x2 
+        b = depth x2
 depth (ExpSub x1 x2) = max (depth x1) (depth x2 + 1)
 depth (ExpDiv x1 x2) = max (depth x1) (depth x2 + 1)
 
@@ -70,14 +83,14 @@ isPrint (x:s) = case x of
     (SExp exp) -> 2
 isPrint _ = 0
 
-gen s = let Ok (Prog e) = pProgram (myLexer s) 
-    in generate e (snd (locals e [] 0)) []
+gen s = let Ok (Prog e) = pProgram (myLexer s)
+    in generate e (snd (locals e [] 1)) []
 
-stack s = let Ok (Prog e) = pProgram (myLexer s) 
+stack s = let Ok (Prog e) = pProgram (myLexer s)
     in max (checkStackSize e 0) (isPrint e)
 
-countLocals s = let Ok (Prog e) = pProgram (myLexer s) 
-    in max (fst (locals e [] 0)) 1
+countLocals s = let Ok (Prog e) = pProgram (myLexer s)
+    in max (fst (locals e [] 1)) 1
 
 joinAll stack_size locals rest name = [".class public " ++ name,
         ".super java/lang/Object",
@@ -96,4 +109,4 @@ joinAll stack_size locals rest name = [".class public " ++ name,
 main = do
     name <- getArgs
     contents <- getContents
-    mapM_ putStrLn (joinAll (stack contents) (countLocals contents) (gen contents) (head name))    
+    mapM_ putStrLn (joinAll (stack contents) (countLocals contents) (gen contents) (head name))
